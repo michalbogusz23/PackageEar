4b2478cb5c18082df50a6bc70ee185a40d43dc96
MichalBogusz <mbogusz998@gmail.com>
2020-10-27 17:41:23 +0100

    added base html template using jinja

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = False
 
+@app.route('/sender/sign-up')
+def signUpUser():
+    return render_template("register_form.html")
+
 @app.route('/')
 def index():
     return render_template("index.html")
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
