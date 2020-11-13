7e1b85acb34832559db316a1f1f52dfac3ce94cf
MichalBogusz <mbogusz998@gmail.com>
2020-11-13 22:56:39 +0100

    rebuilded register and app for new field requirements

--- web/app.py
+++ web/app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 app = Flask(__name__)
 app.debug = False
-
-@app.route('/sender/sign-up')
-def signUpUser():
-    return render_template("register_form.html")
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/sender/register', methods=['GET'])
+def register_form():
+    return render_template("register_form.html")
+
+@app.route('/sender/register', methods=['POST'])
+def register():
+    return response
+
+@app.route('/sender/login', methods=['GET'])
+def login_form():
+    return render_template("login_form.html")
+
+@app.route('/sender/login', methods=['POST'])
+def login():
+    return response
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
