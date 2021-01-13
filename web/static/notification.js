52413e385c22056c646e645a72e6e382d72aff09
MichalBogusz <mbogusz998@gmail.com>
2021-01-14 00:38:19 +0100

    fixes

--- web/static/notification.js
+++ web/static/notification.js
@@ -1,15 +1,17 @@
 function check_notifications() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-        if(xhr.readyState == 4) {
+        if(xhr.readyState == 4 && xhr.status == 200) {
             console.info(xhr.responseText);
+            setTimeout(check_notifications, 1000);
         }
     };
     xhr.open("GET", "/notifications", true);
-    xhr.timeout = 4000
+    xhr.timeout = 30000
     xhr.ontimeout = function() { 
         console.error("Timeout");
         setTimeout(check_notifications, 1000);
     }
     xhr.send();
 }
+check_notifications();
